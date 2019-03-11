06d3af6d6b5685b131fb5af0f103830b43ab7364
harieswaran98 <46838567+harieswaran98@users.noreply.github.com>
2019-03-11 16:35:36 +0530

    Create amazon.c

---
+++ amazon.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main()
+{
+  int a[100], b,c=2,d,e=8,f=0,g=5,h,i,j,k,l;
+  scanf("%d",&d);
+ 
+  l=1;
+  while(g)
+  {
+ b=2;
+  while(b)
+  {
+      k=l*l;
+      b--;
+  }
+  f=f+(k*12);
+  
+  l++;
+ // g--;
+ // printf("%d\t",f);
+ if(d<=f)
+ {
+     g=0;
+ }
+ else
+ {
+     g++;
+ }
+ if(f>=d)
+ {
+     printf("%d \n",e);
+ }
+ else
+ {
+     
+ c=c+2;   
+ e=c*4;
+ }
+ 
+}
+}
